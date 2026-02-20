82b7521811aad761c859d6e2398c372f72827e18
Kwon_Hyuk_il <gurdlf320@naver.com>
2026-02-20 23:39:01 +0900

    refactor: statsController-router change

--- src/routes/stats.routes.ts
+++ src/routes/stats.routes.ts
@@ -1,15 +1,21 @@
 import { Router } from "express";
 import { authMiddleware } from "../middlewares/auth.middleware";
-import {
-  getLast7DaysCount,
-  getMoodCount,
-  getSummary,
-} from "../controllers/stats.controller";
+import { statsController } from "../controllers/stats.controller";
 
 export const statsRouter = Router();
 
-statsRouter.get("/mood-count", authMiddleware, getMoodCount);
+statsRouter.get("/mood-count", authMiddleware, statsController.getMoodCount);
 
-statsRouter.get("/last-7days", authMiddleware, getLast7DaysCount);
+statsRouter.get(
+  "/last-7days",
+  authMiddleware,
+  statsController.getLast7DaysCount,
+);
 
-statsRouter.get("/summary", authMiddleware, getSummary);
+statsRouter.get("/summary", authMiddleware, statsController.getSummary);
+
+statsRouter.get(
+  "/ja-improvement",
+  authMiddleware,
+  statsController.jaImprovement,
+);
